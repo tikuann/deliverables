07ab2f6589a40a7f99bce3ef0406cb52d9852558
tikuann <2193047@yahoo.co.jp>
2021-12-09 13:24:31 +0900

    微調整２

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -11,31 +11,20 @@ use Illuminate\Http\Request;
  
 class LineMessengerController extends Controller
 {
-    public function webhook(Request $request) {
-        // LINEから送られた内容を$inputsに代入
-        $inputs=$request->all();
- 
-        // そこからtypeをとりだし、$message_typeに代入
-        $message_type=$inputs['events'][0]['type'];
- 
-        // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($message_type=='message') {
-            
-            // replyTokenを取得
-            $reply_token=$inputs['events'][0]['replyToken'];
- 
-            // LINEBOTSDKの設定
-            $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
- 
-            // 送信するメッセージの設定
-            $reply_message='メッセージありがとうございます';
- 
-            // ユーザーにメッセージを返す
-            $reply=$bot->replyText($reply_token, $reply_message);
-            
-            return 'ok';
-        }
+    protected $access_token;
+    protected $channel_secret;
+
+    public function __construct()
+    {
+        // :point_down: アクセストークン
+        $this->access_token = env('LINE_ACCESS_TOKEN');
+        // :point_down: チャンネルシークレット
+        $this->channel_secret = env('LINE_CHANNEL_SECRET');
     }
+    
+    public function webhook(Request $request) {
+        return 'ok';
+    }
+
 }
 
