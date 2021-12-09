7d1d050e01657176bd60d2f653f526270c5b05f9
tikuann <2193047@yahoo.co.jp>
2021-12-10 00:02:12 +0900

    微調整

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -23,68 +23,6 @@ class LineApiController extends Controller
     }
     
     public function webhook(Request $request) {
-    $input = $request->all();
-    // ユーザーがどういう操作を行った処理なのかを取得
-    $type  = $input['events'][0]['type'];
-
-    // タイプごとに分岐
-    switch ($type) {
-        // メッセージ受信
-        case 'message':
-            // 返答に必要なトークンを取得
-            $reply_token = $input['events'][0]['replyToken'];
-            // テスト投稿の場合
-            if ($reply_token == '00000000000000000000000000000000') {
-                Log::info('Succeeded');
-                return;
-            }
-            // Lineに送信する準備
-            $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->access_token);
-            $bot         = new \LINE\LINEBot($http_client, ['channelSecret' => $this->channel_secret]);
-            // LINEの投稿処理
-            $message_data = "メッセージありがとうございます。ただいま準備中です";
-            $response     = $bot->replyText($reply_token, $message_data);
-
-            // Succeeded
-            if ($response->isSucceeded()) {
-                Log::info('返信成功');
-                break;
-            }
-            // Failed
-            Log::error($response->getRawBody());
-            break;
-            break;
-
-        // 友だち追加 or ブロック解除
-        case 'follow':
-            // ユーザー固有のIDを取得
-            $mid = $request['events'][0]['source']['userId'];
-            // ユーザー固有のIDはどこかに保存しておいてください。メッセージ送信の際に必要です。
-            LineUser::updateOrCreate(['line_id' => $mid]);
-            Log::info("ユーザーを追加しました。 user_id = " . $mid);
-            break;
-
-        // グループ・トークルーム参加
-        case 'join':
-            Log::info("グループ・トークルームに追加されました。");
-            break;
-
-        // グループ・トークルーム退出
-        case 'leave':
-            Log::info("グループ・トークルームから退出させられました。");
-            break;
-
-        // ブロック
-        case 'unfollow':
-            Log::info("ユーザーにブロックされました。");
-            break;
-
-        default:
-            Log::info("the type is" . $type);
-            break;
-    }
-
-    return;
 }
 
 }
