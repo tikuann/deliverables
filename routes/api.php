1c3f242762f3168b66fc47ba801ef6387ac4549c
tikuann <2193047@yahoo.co.jp>
2021-12-09 23:36:29 +0900

    微調整

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // LINE メッセージ受信
 Route::post('/api/line/webhook', 'LineMessengerController@webhook');
