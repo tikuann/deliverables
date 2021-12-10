6ef48f233611b51c2ef17ffe9deafb9eefaf1818
tikuann <2193047@yahoo.co.jp>
2021-12-10 21:59:15 +0900

    微調整

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -11,17 +11,7 @@ use Illuminate\Http\Request;
  
 class LineApiController extends Controller
 {
-    protected $access_token;
-    protected $channel_secret;
-
-    public function __construct()
-    {
-        // :point_down: アクセストークン
-        $this->access_token = env('LINE_ACCESS_TOKEN');
-        // :point_down: チャンネルシークレット
-        $this->channel_secret = env('LINE_CHANNEL_SECRET');
-    }
-            // メッセージ送信用
+    // メッセージ送信用
     public function webhook() {
  
         $inputs=$request->all();
