8d4f7dc4582230b4d1945982f88f609a1e9ae151
tikuann <2193047@yahoo.co.jp>
2021-12-10 22:20:40 +0900

    微調整

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 
 // LINE メッセージ受信
-Route::get('/line/webhook', 'LineApiController@webhook');
+Route::post('/line/webhook', 'LineApiController@webhook');
  
 // LINE メッセージ送信用
 Route::post('/line/message', 'LineApiController@message');
