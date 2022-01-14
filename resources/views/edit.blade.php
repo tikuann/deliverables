9c099c3c808a54c124b2b9ec8c61d38a8263bb69
tikuann <2193047@yahoo.co.jp>
2022-01-15 02:14:07 +0900

    edit改良

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -19,15 +19,15 @@
             @method('PUT')
             <div class='content__title'>
                 <h2>タイトル</h2>
-                <input type="textarea" name='recipe[title]' value="{{ $recipe->title }}">
+                <textarea name='recipe[title]' rows="4" cols="40" value="{{ $recipe->title }}">
             </div>
             <div class='content__material'>
                 <h2>材料</h2>
-                <input type="textarea" name='recipe[material]' value="{{ $recipe->material }}">
+                <textarea name='recipe[material]' rows="4" cols="40" value="{{ $recipe->material }}">
             </div>
             <div class="content__procedure">
                 <h4>手順</h4>
-                <textarea name='recipe[procedure]' value="{{ $recipe->procedure}}">
+                <textarea name='recipe[procedure]' rows="4" cols="40" value="{{ $recipe->procedure}}">
             </div>
             <div class="quote">
                 <h5>引用</h4>
